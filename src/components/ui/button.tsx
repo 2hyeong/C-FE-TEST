8b91abdff6da764472fd7c94854eefd1166c83a6
2hyeong <doo.hyeongnim@gmail.com>
2023-02-01 22:44:28 +0900

    chore: add button ui

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,9 +1,30 @@
+import type { palette } from "@/types/emotion";
 import { ReactNode } from "react";
 
-interface ButtonProps {
+interface ButtonProps
+  extends Omit<React.HTMLAttributes<HTMLDivElement>, "css"> {
   children: ReactNode;
+  color?: palette;
 }
 
-export default function Button({ children }: ButtonProps) {
-  return <button>{children}</button>;
+export default function Button({
+  children,
+  color = "primary",
+  className,
+}: ButtonProps) {
+  return (
+    <button
+      css={(theme) => ({
+        ...(color === "secondary" ? theme.text.secondary : { color: "white" }),
+        backgroundColor: theme.palette[color],
+        borderRadius: "8px",
+        padding: "13px 20px",
+        border: 0,
+        cursor: "pointer",
+      })}
+      className={className}
+    >
+      {children}
+    </button>
+  );
 }
