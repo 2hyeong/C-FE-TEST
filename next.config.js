fd5e03797724fd19ec1fd0f6145e99397e9ef58c
2hyeong <doo.hyeongnim@gmail.com>
2023-02-01 21:05:34 +0900

    chore: env

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    API_KEY: process.env.NEXT_PUBLIC_API_KEY,
+    KAKAO_API: process.env.NEXT_PUBLIC_KAKAO_API,
+  },
 };
 
 module.exports = nextConfig;
