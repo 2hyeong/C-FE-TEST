b7504e9bf050c50aad384a29730f1963adb3d012
2hyeong <doo.hyeongnim@gmail.com>
2023-02-01 21:06:15 +0900

    chore: react-query, recoil provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,29 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "@emotion/react";
 import { theme } from "@/styles/theme";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import { RecoilRoot } from "recoil";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 0,
+      staleTime: Infinity,
+    },
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      {/* The rest of your application */}
+      <ReactQueryDevtools initialIsOpen={false} />
+      <RecoilRoot>
+        <ThemeProvider theme={theme}>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </RecoilRoot>
+    </QueryClientProvider>
   );
 }
