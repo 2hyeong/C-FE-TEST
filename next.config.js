94e5879aa56f21b994c038056896e481cdb5841e
2hyeong <doo.hyeongnim@gmail.com>
2023-02-01 22:43:13 +0900

    chore: allow images from kakao domain

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const nextConfig = {
     API_KEY: process.env.NEXT_PUBLIC_API_KEY,
     KAKAO_API: process.env.NEXT_PUBLIC_KAKAO_API,
   },
+  images: {
+    domains: ["search1.kakaocdn.net"],
+  },
 };
 
 module.exports = nextConfig;
