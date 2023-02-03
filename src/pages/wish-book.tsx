d60952cc36ddb8269675684d18969bdcb79e1e9f
2hyeong <doo.hyeongnim@gmail.com>
2023-02-03 11:37:51 +0900

    feat: wish list page

--- src/pages/wish-book.tsx
+++ src/pages/wish-book.tsx
@@ -1,28 +1,25 @@
-//
 import Head from "next/head";
-// emotion
-import styled from "@emotion/styled";
 // components
 import Layout from "@/components/layout";
-import SearchBookInput from "@/components/book/search/input";
-import SearchBookList from "@/components/book/search/list";
+// emotion
+import styled from "@emotion/styled";
+import WishBookList from "@/components/book/wish/list";
 
-export default function Home() {
+export default function WishList() {
   return (
     <>
       <Head>
-        <title>도서 검색</title>
-        <meta name="description" content="도서 검색" />
+        <title>내가 찜한 책</title>
+        <meta name="description" content="내가 찜한 책" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Layout>
         <Container>
           <검색Container>
-            <Title2>도서 검색</Title2>
-            <SearchBookInput />
+            <Title2>내가 찜한 책</Title2>
           </검색Container>
-          <SearchBookList />
+          <WishBookList />
         </Container>
       </Layout>
     </>
